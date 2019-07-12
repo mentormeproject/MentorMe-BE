d1c8ada4c8ff1a7b444190e82f8367728fd4ae2e
cpscott1 <thacam18@gmail.com>
2019-07-11 23:36:43 -0500

    added migrations

---
+++ data/migrations/20190711230500_questions.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('questions', tbl => {
+    tbl.increments();
+    tbl
+    .string('title', 128)
+    .notNullable();
+    tbl
+    .string('question', 128)
+    .notNullable();
+    tbl
+    .string('business type', 128)
+    .notNullable()
+    tbl
+    .integer('user_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('users')
+    .onDelete('RESTRICT')
+    .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('questions');
+};
