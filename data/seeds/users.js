d6d0248c050764848d0ceadfa8732948621da243
James Wigdor <jpwigdor@gmail.com>
2019-07-15 14:17:23 -0400

    Add Seeds
    Added seeding to the Users and Questions table

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -1,13 +1,25 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("table_name")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("table_name").insert([
+        {
+          id: 1,
+          username: "Batman",
+          password: "test"
+        },
+        {
+          id: 2,
+          username: "Rick",
+          password: "test"
+        },
+        {
+          id: 3,
+          username: "Tony Stark",
+          password: "test"
+        }
       ]);
     });
 };
