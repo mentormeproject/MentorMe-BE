125752cec15ed4161e8a81d3b4f82761e83bc89f
James Wigdor <jpwigdor@gmail.com>
2019-07-17 22:57:38 -0400

    updating migration tables
    it now includes additional fields for the Users

--- data/migrations/20190715155613_questions_correct_type.js
+++
@@ -1,9 +0,0 @@
-exports.up = function(knex, promise) {
-  return knex.schema.table("questions", tbl => {
-    tbl.renameColumn("business type", "business_type");
-  });
-};
-
-exports.down = function(knex, promise) {
-  return knex.schema.dropColumn("business_type");
-};
